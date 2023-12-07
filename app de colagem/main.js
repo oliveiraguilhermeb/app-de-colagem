5a47134570578f9f2d85df06552532f021f115fa
oliveiraguilhermeb <125418140+oliveiraguilhermeb@users.noreply.github.com>
2023-12-06 21:11:17 -0300

    Add files via upload

---
+++ app de colagem/main.js
@@ -0,0 +1,50 @@
+const camera = document.getElementById("camera");
+const result = document.getElementById("result");
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+Webcam.attach(camera);
+
+function snap() {
+    result.innerHTML = "";
+    Webcam.snap(function (dataURI) {
+        const captura = document.createElement("img");
+        captura.setAttribute("id", "captura");
+        captura.setAttribute("src", dataURI);
+        result.appendChild(captura);
+    });
+}
+const teachablemachineLink = '';
+const modelLink = teachablemachineLink + 'model.json'
+const classifier = ml5.imageClassifier(modelLink, modelLoaded)
+
+function modelLoaded() {
+    console.log('Model Carregado')
+    Webcam.attach(camera);
+}
+
+function check() {
+    const img = document.getElementById('captura');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    console.log(error);
+    console.log(results)
+
+    const objeto = document.getElementById("objectName");
+    const precisao = document.getElementById("objectAccuracy");
+
+
+        if (!error) {
+            objeto.innerHTML = results[0].label;
+            preciso.innerHTML = results[0].confidence.toFixed(2);
+        } else {
+            console.error(error);
+        }
+}
